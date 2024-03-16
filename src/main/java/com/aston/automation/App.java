74ee6e63e095de835d8a87948bf4e30a45da767a
Andrei <79132182+ms8895@users.noreply.github.com>
2024-03-16 18:01:01 +0600

    Lesson_7.1

--- src/main/java/com/aston/automation/App.java
+++ src/main/java/com/aston/automation/App.java
@@ -1,13 +1,59 @@
 package com.aston.automation;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+
+        try {
+            // Массив со строкой.
+            String[][] array = {{"1", "2", "3", "4"},
+                    {"5", "6", "7", "8"},
+                    {"9", "J", "11", "12"},
+                    {"13", "14", "15", "16"}};
+            addArray(array);
+            // Массив с числами.
+            String[][] newArray1 = {{"1", "2", "3", "4"},
+                    {"5", "6", "7", "8"},
+                    {"9", "10", "11", "12"},
+                    {"13", "14", "15", "16"}};
+            addArray(newArray1);
+
+            // Массив 3х3.
+            String[][] newArray2 = {{"1", "2", "3", "4"},
+                    {"5", "6", "7", "8"},
+                    {"9", "10", "11", "12"}};
+            addArray(newArray2);
+
+            //Массив с символом.
+            String[][] newArray3 = {{"1", "2", "3", "4"},
+                    {"5", "6", "7", "8"},
+                    {"9", "10", "11", "12"},
+                    {"13", "14", "?", "16"}};
+            addArray(newArray3);
+
+        } catch (MyArraySizeException | MyArrayDataException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static String addArray(String[][] array) throws MyArraySizeException, MyArrayDataException {
+
+        if (array.length != 4) {
+            throw new MyArraySizeException("\n" + "Массив не соответствует размеру 4х4.");
+        } else {
+            int sum = 0;
+
+            for (int i = 0; i < array.length; i++) {
+                for (int j = 0; j < array[i].length; j++) {
+                    try {
+                        sum += Integer.parseInt(array[i][j]);
+                    } catch (NumberFormatException e) {
+                        throw new MyArrayDataException("\n" + "Невозможно преобразовать строку, символ в число."
+                                + "\n" + "Строка, символ: " + array[i][j] + "."
+                                + "\n" + "Индекс строки, символа: " + "[" + i + "]" + "[" + j + "].");
+                    }
+                }
+            }
+            return "Сумма массива: " + sum + ".";
+        }
     }
 }
--- src/test/java/com/aston/automation/AppTest.java
+++ src/test/java/com/aston/automation/AppTest.java
@@ -1,20 +1,96 @@
 package com.aston.automation;
 
-import static org.junit.Assert.assertTrue;
-
+import org.assertj.core.api.Assertions;
 import org.junit.Test;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-{
-    /**
-     * Rigorous Test :-)
-     */
+import static junit.framework.TestCase.assertEquals;
+
+public class AppTest {
+
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    public void testAddArrayString() {
+        // Массив со строкой.
+        String[][] array = {{"1", "2", "3", "4"},
+                {"5", "6", "7", "8"},
+                {"9", "J", "11", "12"},
+                {"13", "14", "15", "16"}};
+
+        Assertions.assertThatThrownBy(() -> App.addArray(array))
+                .isInstanceOf(MyArrayDataException.class)
+                .hasMessage("\n" +
+                        "Невозможно преобразовать строку, символ в число.\n" +
+                        "Строка, символ: J.\n" +
+                        "Индекс строки, символа: [2][1].");
+    }
+
+    @Test
+    public void testAddArrayStringDataException() {
+        // Массив со строкой.
+        String[][] array = {{"1", "2", "3", "4"},
+                {"5", "6", "7", "8"},
+                {"9", "J", "11", "12"},
+                {"13", "14", "15", "16"}};
+
+        Assertions.assertThatThrownBy(() -> App.addArray(array)).isInstanceOf(MyArrayDataException.class);
+    }
+
+    @Test
+    public void testAddArraySimbol() {
+        //Массив с символом.
+        String[][] array = {{"1", "2", "3", "4"},
+                {"5", "6", "7", "8"},
+                {"9", "10", "11", "12"},
+                {"13", "14", "?", "16"}};
+
+        Assertions.assertThatThrownBy(() -> App.addArray(array))
+                .isInstanceOf(MyArrayDataException.class)
+                .hasMessage("\n" +
+                        "Невозможно преобразовать строку, символ в число.\n" +
+                        "Строка, символ: ?.\n" +
+                        "Индекс строки, символа: [3][2].");
+    }
+
+    @Test
+    public void testAddArraySimbolDataException() {
+        //Массив с символом.
+        String[][] array = {{"1", "2", "3", "4"},
+                {"5", "6", "7", "8"},
+                {"9", "10", "11", "12"},
+                {"13", "14", "?", "16"}};
+
+        Assertions.assertThatThrownBy(() -> App.addArray(array)).isInstanceOf(MyArrayDataException.class);
+    }
+
+    @Test
+    public void testAddArrayNumbers() throws MyArraySizeException, MyArrayDataException {
+        // Массив с числами.
+        String[][] array = {{"1", "2", "3", "4"},
+                {"5", "6", "7", "8"},
+                {"9", "10", "11", "12"},
+                {"13", "14", "15", "16"}};
+
+        assertEquals("Сумма массива: 136.", App.addArray(array));
+    }
+
+    @Test
+    public void testAddArraySize() {
+        // Массив 3х3.
+        String[][] array = {{"1", "2", "3", "4"},
+                {"5", "6", "7", "8"},
+                {"9", "10", "11", "12"}};
+
+        Assertions.assertThatThrownBy(() -> App.addArray(array))
+                .isInstanceOf(MyArraySizeException.class)
+                .hasMessage("\n" + "Массив не соответствует размеру 4х4.");
+    }
+
+    @Test
+    public void testAddArraySizeException() {
+        // Массив 3х3.
+        String[][] array = {{"1", "2", "3", "4"},
+                {"5", "6", "7", "8"},
+                {"9", "10", "11", "12"}};
+
+        Assertions.assertThatThrownBy(() -> App.addArray(array)).isInstanceOf(MyArraySizeException.class);
     }
 }
