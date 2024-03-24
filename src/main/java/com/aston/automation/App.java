fb82605a408f209698bd9ef95dc6c07f3747026f
Andrei <79132182+ms8895@users.noreply.github.com>
2024-03-24 12:06:07 +0600

    Lesson_8.1

--- src/main/java/com/aston/automation/App.java
+++ src/main/java/com/aston/automation/App.java
@@ -1,13 +1,51 @@
 package com.aston.automation;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class App {
+    public static void main(String[] args) {
+        // 1.
+        System.out.println("1.");
+        ArrayList<String> arrayList = new ArrayList<>(Arrays.asList(
+                "apple", "banana", "cat", "dog", "elephant",
+                "fish", "grape", "horse", "orange", "pear",
+                "pineapple", "rabbit", "apple", "cat", "dog",
+                "elephant", "fish", "grape", "horse", "orange"));
+
+        // Вывод уникальных строк.
+        Collections.sort(arrayList);
+        List<String> listWithoutDuplicates = arrayList.stream().distinct().collect(Collectors.toList());
+        System.out.println(listWithoutDuplicates);
+
+        // Подсчет строк.
+        Collections.sort(arrayList);
+        ArrayList<String> arrayFree = new ArrayList<>();
+
+        System.out.println();
+
+        for (String item : arrayList) {
+            int occurrences = Collections.frequency(arrayList, item); // Узнаём количество "яблок"
+            if (!arrayFree.contains(item)) {
+                System.out.println("В списке: " + item + " = " + occurrences);
+                arrayFree.add(item);
+            }
+        }
+
+        // 2.
+        System.out.println("\n" + "2.");
+
+        List<Contact> contacts = new ArrayList<>(Arrays.asList(
+                new Contact("Иванов", 111),
+                new Contact("Сидоров", 333),
+                new Contact("Петров", 222)));
+
+        // Добавление контакта.
+        contacts.add(new Contact("Иванов", 444));
+        // Вывод контакта
+        Contact.getContact(contacts, "Иванов");
     }
 }
