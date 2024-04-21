b7a0682db912fe51f49a9d5ec1111bb7e89fa7fd
Andrei <79132182+ms8895@users.noreply.github.com>
2024-04-21 18:34:35 +0600

    Lesson_12_testng. add dependency for testng.

--- src/test/java/com/aston/automation/AppTest.java
+++ src/test/java/com/aston/automation/AppTest.java
@@ -1,8 +1,8 @@
 package com.aston.automation;
 
-import org.junit.jupiter.api.Test;
+import org.testng.annotations.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.testng.AssertJUnit.assertEquals;
 
 public class AppTest {
 
