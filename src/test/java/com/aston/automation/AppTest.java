f810f6e98a8a57c3169ff912156d8246b7ab33ac
Andrei <79132182+ms8895@users.noreply.github.com>
2024-05-07 13:39:12 +0600

    Lesson_13_1. Added parameterized test.

--- src/test/java/com/aston/automation/AppTest.java
+++ src/test/java/com/aston/automation/AppTest.java
@@ -1,28 +1,14 @@
 package com.aston.automation;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class AppTest {
-
-    @Test
-    public void factorialIsGreaterThanOneTest() {
-        assertEquals(3628800, App.getFactorial(10));
-    }
-
-    @Test
-    public void factorialOfZeroTest() {
-        assertEquals(1, App.getFactorial(0));
-    }
-
-    @Test
-    public void factorialOfOneTest() {
-        assertEquals(1, App.getFactorial(1));
-    }
-
-    @Test
-    public void factorialIsLessThanZeroTest() {
-        assertEquals(0, App.getFactorial(-1));
+    @ParameterizedTest
+    @CsvSource({"10, 3628800", "0, 1", "1,1", "-1,0"})
+    public void factorialTests(int input, int expected) {
+        assertEquals(expected, App.getFactorial(input));
     }
 }
